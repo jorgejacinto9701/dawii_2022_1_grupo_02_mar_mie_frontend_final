fe295ca31c47804b932dae8f0e30a1586cb21b95
CarlosCardenas99 <revocarlos99@gmail.com>
2022-06-19 11:52:59 -0500

    PC1-Cardenas

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,24 +2,35 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppSettings } from '../app.settings';
+import { Ubigeo } from '../models/ubigeo.model';
+import { NumberSymbol } from '@angular/common';
 import { Cliente } from '../models/cliente.model';
 
 const baseUrlUtil = AppSettings.API_ENDPOINT+ '/util';
 const baseUrlCliente = AppSettings.API_ENDPOINT+ '/cliente';
+const baseConsultarCliente="http://localhost:8090/url/cliente";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  listaClienteAll(nombres:string, dni:string, idUbigeo:NumberSymbol, estado:number):Observable<any> {
+    const params= new HttpParams().set("nombres",nombres).set("dni", dni).set("idUbigeo", idUbigeo).set("estado",estado);
+    return  this.http.get<any>(baseConsultarCliente+"/listaClienteConParametros",{params});
+     }
 
   
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   listaCliente():Observable<Cliente[]>{
     return this.http.get<Cliente[]>(baseUrlUtil+"/listaCliente");
   }
 
-  
 
-  
+  registraCliente(data:Cliente) : Observable<any>{
+    return this.http.post(baseUrlCliente,data);
+
+
+}
 }
