d3917cd9f43d6bb208758e8ec11c2cb3b8ea196b
JamberTorres <jamber_torres199@hotmail.com>
2022-06-19 13:51:15 -0500

    PC Torres CrudProveedor

--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -7,7 +7,8 @@ import { AppSettings } from '../app.settings';
 
 const baseUrlUtil = AppSettings.API_ENDPOINT+ '/util';
 const baseUrlProveedor = AppSettings.API_ENDPOINT+ '/proveedor';
-
+const baseUrlProveedorCrud = AppSettings.API_ENDPOINT+ '/crudProveedor';
+//const baseUrl = 'http://localhost:8090/rest/crudProveedor';
 
 @Injectable({
   providedIn: 'root'
@@ -29,5 +30,18 @@ listaProveedorAll(razonsocial:string, ruc:string, idUbigeo:number, estado:number
   const params= new HttpParams().set("razonsocial",razonsocial).set("ruc", ruc).set("idUbigeo", idUbigeo).set("estado",estado);
   return  this.http.get<any>(baseUrlProveedor+"/listaProveedorConParametros",{params});
    }
-
+ //crud
+ consultaProveedor(filtro : string) : Observable<Proveedor[]>{
+  return this.http.get<Proveedor[]>(baseUrlProveedorCrud + "/porRazonsocial/" + filtro);
+          }
+  registraProveedor(obj: Proveedor): Observable<any>{
+  return this.http.post(baseUrlProveedorCrud + "/registraProveedor", obj);
+   }
+  
+   actualizaProveedor(obj: Proveedor): Observable<any>{
+    return this.http.put(baseUrlProveedorCrud + "/actualizaProveedor", obj);
+     } 
+     eliminaProveedor(id : any): Observable<any>{
+      return this.http.delete(baseUrlProveedorCrud + "/eliminaProveedor/"+ id);
+       } 
 }
